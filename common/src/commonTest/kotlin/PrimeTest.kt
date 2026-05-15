68747715cc2e3760519f7ed5c6747313cdef5d25
Marc Reichelt <mcreichelt@gmail.com>
2026-05-15 14:59:51 +0200

    Add tests for prime numbers

---
+++ common/src/commonTest/kotlin/PrimeTest.kt
@@ -0,0 +1,44 @@
+package io.github.kotlin.fibonacci
+
+import kotlin.test.Test
+import kotlin.test.assertContentEquals
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
+
+class PrimeTest {
+
+    @Test
+    fun `test first 4 numbers`() {
+        assertFalse(isPrime(1))
+        assertTrue(isPrime(2))
+        assertTrue(isPrime(3))
+        assertFalse(isPrime(4))
+    }
+
+    @Test
+    fun `test first 20 primes`() {
+        val expected = listOf(2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71)
+            .map { it.toLong() }
+        val actual = (1L..71L).filter { isPrime(it) }
+        assertContentEquals(expected, actual)
+    }
+
+    @Test
+    fun `test big prime`() {
+        assertTrue(isPrime(92233720368547421L))
+    }
+
+    @Test
+    fun `test bigger prime`() {
+        assertTrue(isPrime(922337203685474371L))
+        assertFalse(isPrime(922337203685474373L))
+    }
+
+
+    @Test
+    fun `long computation`() {
+        assertTrue(isPrime(9223370998999999967L))
+        assertFalse(isPrime(9223370998999999969L))
+    }
+
+}
