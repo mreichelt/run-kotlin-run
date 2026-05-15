c69c60e1ee903eabe1126e4bdfb502ee75ba7afa
Marc Reichelt <mcreichelt@gmail.com>
2026-05-15 15:10:21 +0200

    Simplify Main.kt

--- 01_jvm/src/jvmMain/kotlin/Main.kt
+++ 01_jvm/src/jvmMain/kotlin/Main.kt
@@ -3,6 +3,6 @@ import io.github.kotlin.fibonacci.isPrime
 fun main(args: Array<String>) {
     for (arg in args) {
         val n = arg.toLong()
-        println("$n " + if (isPrime(n)) "is a prime" else "is NOT a prime")
+        println("$n" + if (isPrime(n)) " is a prime" else "")
     }
 }
